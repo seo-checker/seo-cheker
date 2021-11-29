12ad991a22846fa4bf7fdf1e1258f7cc89eee16c
Alexey Ponomarev <wayfinder.tm@gmail.com>
2021-11-29 17:52:02 +0400

    Replaced schema validation to chai

--- src/blocks/Schema.js
+++ src/blocks/Schema.js
@@ -1,4 +1,4 @@
-const isValidJSON = (json) => {
+const getJSON = (json) => {
     try {
         return JSON.parse(json);
     } catch (e) {
@@ -6,14 +6,7 @@ const isValidJSON = (json) => {
     }
 }
 
-const isSchema = (obj) => {
-    const regex = /^http(s)?:\/\/schema.org\//
-    return regex.test(obj['@context']);
-}
-
-const getType = (obj) => {
-    return obj['@type'] ?? false;
-}
+const schemaRegex = /^http(s)?:\/\/schema.org\/$/;
 
 const Schema = {
     slug: "schema",
@@ -36,70 +29,65 @@ const Schema = {
                             slug: 'valid',
                             name: "All JSON-LD has valid JSON format",
                             func: (el, expect) => {
-                                const results = el.map(item => {
+                                el.map(item => {
                                     const content = item.innerText;
-                                    return isValidJSON(content) ? 'valid' : 'invalid';
+                                    const obj = getJSON(content);
+                                    expect(obj).to.be.an.instanceof(Object);
+                                    expect(obj).to.have.property('@context').and.match(schemaRegex);
+                                    expect(obj).to.have.property('@type').and.not.to.be.empty;
                                 });
-                                expect(results).not.to.include('invalid');
                             }
                         },
                         {
                             slug: 'breadcrumbs',
-                            name: "Breadcrumbs JSON-LD exist",
+                            name: "Breadcrumbs JSON-LD exist and there is only one breadcrumbs block",
                             iswarning: true,
                             func: (el, expect) => {
                                 const results = el.map(item => {
-                                    const content = item.innerText;
-                                    const obj = isValidJSON(content);
-                                    return !!(obj && isSchema(obj) && getType(obj) == 'BreadcrumbList');
+                                    try {
+                                        const content = item.innerText;
+                                        const obj = getJSON(content);
+                                        expect(obj).to.be.an.instanceof(Object);
+                                        expect(obj).to.have.property('@context').and.match(schemaRegex);
+                                        expect(obj).to.have.property('@type').to.be.equal('BreadcrumbList');
+                                        return true;
+                                    } catch(e) {
+                                        return false;
+                                    }
                                 }).filter(Boolean);
                                 expect(results).not.to.be.empty;
+                                expect(results).to.have.lengthOf(1);
                             },
                             tests: [
-                                {
-                                    slug: 'one',
-                                    name: "There is only one breadcrumbs JSON-LD on page",
-                                    func: (el, expect) => {
-                                        const results = el.map(item => {
-                                            const content = item.innerText;
-                                            const obj = isValidJSON(content);
-                                            return !!(getType(obj) == 'BreadcrumbList');
-                                        }).filter(Boolean);
-                                        expect(results).to.have.lengthOf(1);
-                                    },
-                                },
                                 {
                                     slug: 'valid',
                                     name: "Breadcrumbs is valid",
                                     func: (el, expect) => {
-                                        const results = el.map(item => {
-                                            const content = item.innerText;
-                                            const obj = isValidJSON(content);
-                                            const items = obj['itemListElement'] ?? [];
-                                            let pos = 1;
-                                            if (
-                                                !obj ||
-                                                !isSchema(obj) ||
-                                                getType(obj) != 'BreadcrumbList' ||
-                                                !Array.isArray(items) ||
-                                                !items.length
-                                            ) return false;
-                                            items.sort(function(a, b) {
-                                                return parseInt(a['position']) - parseInt(b['position']);
-                                            });
-                                            for (let i = 0; i < items.length; i++) {
-                                                const item = items[i];
-                                                if (
-                                                    item['@type'] !== 'ListItem' ||
-                                                    parseInt(item['position']) !== pos ||
-                                                    !item['name'] ||
-                                                    !item['item']
-                                                ) return false;
-                                                pos++;
+                                        const breadcrumbs = el.map(item => {
+                                            try {
+                                                const content = item.innerText;
+                                                const obj = getJSON(content);
+                                                expect(obj).to.be.an.instanceof(Object);
+                                                expect(obj).to.have.property('@context').and.match(schemaRegex);
+                                                expect(obj).to.have.property('@type').to.equal('BreadcrumbList');
+                                                return obj;
+                                            } catch(e) {
+                                                return false;
                                             }
-                                            return true;
                                         });
-                                        expect(results).not.to.include(false);
+                                        const obj = breadcrumbs[0];
+                                        expect(obj).to.have.property('itemListElement').and.to.be.an.instanceof(Array);
+                                        const list = obj['itemListElement'];
+                                        expect(list).not.to.be.empty;
+                                        let pos = 1;
+                                        list.map(item => {
+                                            expect(item).to.be.an.instanceof(Object);
+                                            expect(item).to.have.property('@type').to.equal('ListItem');
+                                            expect(item).to.have.property('position').to.be.equal(pos);
+                                            expect(item).to.have.property('name').and.not.to.be.empty;
+                                            expect(item).to.have.property('item').and.not.to.be.empty;
+                                            pos++;
+                                        });
                                     },
                                 }
                             ]
@@ -123,53 +111,37 @@ const Schema = {
                     tests: [
                         {
                             slug: 'breadcrumbs',
-                            name: "Breadcrumbs microdata exist",
+                            name: "Breadcrumbs microdata exist and there is only one breadcrumbs microdata on page",
                             selector: '[itemtype*="BreadcrumbList"]',
                             iswarning: true,
                             func: (el, expect) => {
                                 expect(el).not.to.be.empty;
-                                const regex = /^http(s)?:\/\/schema.org\/BreadcrumbList$/;
-                                const results = el.map(item => regex.test(item?.getAttribute('itemtype') ?? ''));
-                                expect(results).not.to.include(false);
+                                const results = el.map(item => {
+                                    expect(item.getAttribute('itemtype')).to.match(/^http(s)?:\/\/schema.org\/BreadcrumbList$/);
+                                    return true;
+                                }).filter(Boolean);
+                                expect(results).not.to.be.empty;
+                                expect(results).to.have.lengthOf(1);
                             },
                             tests: [
-                                {
-                                    slug: 'one',
-                                    name: "There is only one breadcrumbs microdata on page",
-                                    func: (el, expect) => {
-                                        expect(el).to.have.lengthOf(1);
-                                    },
-                                },
                                 {
                                     slug: 'valid',
                                     name: "Breadcrumbs microdata is valid",
                                     func: (el, expect) => {
-                                        const results = el.map(item => {
-                                            const items = item.querySelectorAll('[itemprop="itemListElement"][itemscope]');
-                                            if (!items.length)
-                                                return false;
-
-                                            for (let pos = 1; pos <= items.length; pos++) {
-                                                const item = items[pos - 1];
-                                                const itemType = item?.getAttribute('itemtype') ?? '';
-                                                const regex = /^http(s)?:\/\/schema.org\/ListItem$/;
-                                                if (!regex.test(itemType))
-                                                    return false;
-
-                                                const itemItem = item.querySelector('a[itemprop="item"]') ?? (items.length === pos); // Last item may not have an item element
-                                                const itemName = item.querySelector('[itemprop="name"]')?.innerText ?? null;
-                                                const itemPosition = parseInt(item.querySelector('[itemprop="position"]')?.getAttribute('content') ?? 0);
-
-                                                if (
-                                                    !itemItem ||
-                                                    !itemName ||
-                                                    itemPosition !== pos
-                                                )
-                                                    return false;
-                                            };
-                                            return true;
-                                        });
-                                        expect(results).not.to.include(false);
+                                        const obj = el[0];
+                                        const items = obj.querySelectorAll('[itemprop="itemListElement"][itemscope]');
+                                        expect(items).to.be.an.instanceof(NodeList).and.not.to.be.empty;
+                                        let pos = 1;
+                                        for (let pos = 1; pos <= items.length; pos++) {
+                                            const item = items[pos - 1];
+                                            const itemItem = (!!item.querySelector('a[itemprop="item"]') || (items.length === pos)); // Last item may not have an item element
+                                            const itemName = item.querySelector('[itemprop="name"]')?.innerText ?? null;
+                                            const itemPosition = parseInt(item.querySelector('[itemprop="position"]')?.getAttribute('content') ?? 0);
+                                            console.log(itemItem);
+                                            expect(itemItem).to.be.true;
+                                            expect(itemName).not.to.be.empty;
+                                            expect(itemPosition).to.be.equal(pos);
+                                        };
                                     },
                                 }
                             ]
